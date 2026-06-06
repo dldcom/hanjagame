40dfeaa2d0a3d04f183128b02a241522831f9dea
dldcom <dldcom@gmail.com>
2026-06-06 14:24:53 +0900

    Securely use GitHub Secrets for Supabase and refactor code

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mgwvclrytjtmbvjgnjdl.supabase.co';
-const supabaseKey = 'sb_publishable_VB2qjoXbNEcvX_R6lt6_1g_XiIqwhPf';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
